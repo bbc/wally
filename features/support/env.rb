2568035365dd09a020fe26804d31d7893540737d
Andrew Vos <andrew.vos@gmail.com>
2012-02-07 09:58:51 +0000

    Added headless gem so I can run this under my ubuntu devbox

--- features/support/env.rb
+++ features/support/env.rb
@@ -12,6 +12,20 @@ After do
   Wally::Project.delete_all
 end
 
+require 'headless'
+headless = Headless.new
+at_exit do
+  headless.destroy
+end
+
+Before("@selenium,@javascript", "~@no-headless") do
+  headless.start if Capybara.current_driver == :selenium
+end
+
+After("@selenium,@javascript", "~@no-headless") do
+  headless.stop if Capybara.current_driver == :selenium
+end
+
 def project name
   project = Wally::Project.first(:name => name)
   unless project
--- wally.gemspec
+++ wally.gemspec
@@ -30,4 +30,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency "rspec"
   s.add_development_dependency "fakefs"
   s.add_development_dependency "launchy"
+  s.add_development_dependency "headless"
 end
