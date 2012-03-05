4be2285527747ecc0e0b9a0724cc1fc83f8f9021
Andrew Vos <andrew.vos@gmail.com>
2012-03-05 11:21:18 +0000

    Fixed broken feature that was pointing to the wrong project url

--- features/step_definitions/project_home_steps.rb
+++ features/step_definitions/project_home_steps.rb
@@ -1,8 +1,8 @@
 Then /^I should see a link to my sample features$/ do
-  page.should have_link "Kate Moss", :href => "/project/features/kate-moss"
-  page.should have_link "Katie Price", :href => "/project/features/katie-price"
-  page.should have_link "Jessica-Jane Clement", :href => "/project/features/jessica-jane-clement"
-  page.should have_link "Elle Macpherson", :href => "/project/features/elle-macpherson"
+  page.should have_link "Kate Moss", :href => "/projects/project/features/kate-moss"
+  page.should have_link "Katie Price", :href => "/projects/project/features/katie-price"
+  page.should have_link "Jessica-Jane Clement", :href => "/projects/project/features/jessica-jane-clement"
+  page.should have_link "Elle Macpherson", :href => "/projects/project/features/elle-macpherson"
 end
 
 Then /^the features are ordered alphabetically$/ do
