5cdf90d3c2085e891fa85c1e591659b2907e9957
Lenny Marks <lenny@aps.org>
2012-08-01 13:20:10 -0400

    Initial code for importing and retrieving project artifacts
    Project
      #import_content(path, io) imports generated project artifacts
        a first level .feature
          adds the feature to the project
          adds a topic for the feature
          links the feature with and the topic
        a nested .feature
          adds a topic for each parent element in the feature path
      #topic(path) looks up a topic by path
        finds top level topic
        finds nested topics
        returns nil when no topic for path is found
        finds topics using :'s or /'s for separators

--- lib/wally/project.rb
+++ lib/wally/project.rb
@@ -1,8 +1,51 @@
+require 'wally/topic'
+
 module Wally
   class Project
     include MongoMapper::Document
-
+    
     key :name, String
     many :features, :class => Wally::Feature
+    many :topics, :class => Wally::Topic
+    
+    class << self
+      def find_by_name(name)
+        Wally::Project.first(:name => name)
+      end
+    end
+    
+    def topic(topic_path)
+      topic_path = topic_path.gsub(':', '/')
+      topics.detect do |t| 
+        break t if (t = t.topic(topic_path))
+      end
+    end
+
+    def import_content(path, io)
+      topic = ensure_topics(path)
+      if path.match(/\.feature$/)
+        features << feature = Feature.parse_feature(path, io)
+        topic.link_feature(feature)
+      end
+    end
+        
+    def clear_features
+      features.clear
+      topics.clear
+    end
+    
+    def to_param
+      name
+    end
+    
+    private
+    
+    def ensure_topics(path)
+      root_name = path.split('/').first
+      unless (topic = topic(root_name))
+        topics << topic = Topic.new(:path => root_name)
+      end
+      topic.ensure_descendents(path)
+    end
   end
 end
--- spec/wally/project_spec.rb
+++ spec/wally/project_spec.rb
@@ -10,9 +10,70 @@ module Wally
 
     it "stores features" do
       Project.create({:name => "project2", :features => [
-        Feature.new(:path => "feature/path")
+          Feature.new(:path => "feature/path")
       ]})
       Project.first.features.first.path.should == "feature/path"
+    end
+    
+    describe '#import_content(path, io)', 'imports generated project artifacts' do
+      describe 'a first level .feature' do
+        before do
+          Feature.stub(:parse_feature).and_return(Feature.new)
+        end
+        
+        it 'adds the feature to the project' do
+          feature_io = StringIO.new('Feature: A feature')
+          Feature.should_receive(:parse_feature).with('a.feature', feature_io).and_return(Feature.new)
+          subject.import_content('a.feature', feature_io)
+        end
+        
+        it 'adds a topic for the feature' do
+          subject.import_content('a.feature', StringIO.new)
+          subject.topic('a').should_not be_nil
+        end
+        
+        it 'links the feature with and the topic' do
+          feature = double('Feature', :id => 'featureid').as_null_object
+          Feature.stub(:parse_feature).and_return(feature)
+          subject.import_content('a.feature', StringIO.new)
+          subject.topic('a').feature_id.should == 'featureid'
+        end
+      end
+      
+      describe 'a nested .feature' do
+        it 'adds a topic for each parent element in the feature path' do
+          subject.import_content('foo/bar/baz/a.feature', StringIO.new)
+          subject.topic('foo').should_not be_nil
+          subject.topic('foo/bar').should_not be_nil
+          subject.topic('foo/bar/baz').should_not be_nil
+          subject.topic('foo/bar/baz/a').should_not be_nil
+        end
+      end
+    end
+
+    describe '#topic(path)', 'looks up a topic by path' do
+      it 'finds top level topic' do
+        subject.topics << Topic.new(:path => 'foo')
+        subject.topic('foo').path.should == 'foo'
+      end
+      
+      it 'finds nested topics' do
+        subject.topics << foo = Topic.new(:path => 'foo')
+        foo.children << foo_bar = Topic.new(:path => 'foo/bar')
+        foo_bar.children << Topic.new(:path => 'foo/bar/baz')
+        subject.topic('foo/bar/baz').path.should == 'foo/bar/baz'
+      end
+      
+      it 'returns nil when no topic for path is found' do
+        subject.topic('foo').should be_nil
+      end
+      
+      it 'finds topics using :\'s or /\'s for separators' do
+        subject.topics << foo = Topic.new(:path => 'foo')
+        foo.children << foo_bar = Topic.new(:path => 'foo/bar')
+        foo_bar.children << Topic.new(:path => 'foo/bar/baz')
+        subject.topic('foo:bar:baz').path.should == 'foo/bar/baz'
+      end
     end
   end
 end
