492064de14d96cc0c36e358a3c10cffc8c8e996d
Jon Rowe <hello@jonrowe.co.uk>
2013-06-12 23:14:23 +1000

    seperate config

--- lib/wally/application.rb
+++ lib/wally/application.rb
@@ -6,16 +6,9 @@ require "rdiscount"
 
 configure do
   set :haml, { :ugly=>true }
+  require 'wally/config'
 end
 
-if ENV['MONGOHQ_URL']
-  uri = URI.parse(ENV['MONGOHQ_URL'])
-  MongoMapper.connection = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
-  MongoMapper.database = uri.path.gsub(/^\//, '')
-else
-  MongoMapper.connection = Mongo::Connection.new('localhost')
-  MongoMapper.database = "wally"
-end
 
 put '/projects/:project/features/?' do
   error 403 unless authenticated?
